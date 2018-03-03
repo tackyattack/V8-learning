73a50e8db8efb2cdead7a3eb2b46df2a6119fe1b
HENRY BERGIN <tackyattack@gmail.com>
2018-03-02 20:44:58 -0600

    Initial Commit

---
+++ V8_learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  V8_learning
+//
+//  Created by HENRY BERGIN on 3/2/18.
+//  Copyright © 2018 HENRY BERGIN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
